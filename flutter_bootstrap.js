4ca39b2216295955c62c8f76b12f36507bf1e07d
GitHub geungujeon4-lgtm <geungujeon4@gmail.com>
2025-07-28 03:25:33 +0900

    상세페이지 너비 400px 정확히 고정

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3800213801"
+    serviceWorkerVersion: "2602375505"
   }
 });
